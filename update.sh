cfc7573b1e65daeda9dbe0ae4209ef90bc3db145
tntim96 <tntim96@gmail.com>
2014-07-15 21:36:26 +1000

    Initial commit

---
+++ update.sh
@@ -0,0 +1,16 @@
+set -v
+git pull
+cd ../JSCover
+git pull
+cd ../JSCover-gh-pages
+git pull origin gh-pages
+cd ../JSCover-maven-plugin
+git pull
+cd ../JSCover-maven-test
+git pull
+cd ../JSCover-samples
+git pull
+cd ../JMonitaur
+git pull
+
+
