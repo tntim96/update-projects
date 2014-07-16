52d2e6700c0efa8f8c9adc0f17f625d10cded908
tntim96 <tntim96@gmail.com>
2014-07-16 20:25:18 +1000

    Add script to checkout projects

---
+++ checkout.sh
@@ -0,0 +1,14 @@
+set -v
+cd ..
+git clone https://github.com/tntim96/JSCover.git
+git clone https://github.com/tntim96/JSCover.git JSCover-gh-pages
+cd JSCover-gh-pages
+git checkout gh-pages
+cd ..
+git clone https://github.com/tntim96/JSCover-maven-plugin.git
+git clone https://github.com/tntim96/JSCover-maven-test.git
+git clone https://github.com/tntim96/JSCover-samples.git
+git clone https://github.com/tntim96/JMonitaur.git
+git clone https://github.com/tntim96/rhino-jscover.git
+git clone https://github.com/tntim96/rhino-jscover-repackaged.git
+
